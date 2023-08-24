64dcd188e65e1d7ba095ee44b24631702ef889ce
alif-arrizqy <alif.arrizqy20@gmail.com>
2023-08-24 16:46:34 +0700

    add swagger api user

--- user_app/user_app/settings.py
+++ user_app/user_app/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'user_service'
+    'user_service',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- user_app/user_app/urls.py
+++ user_app/user_app/urls.py
@@ -16,9 +16,25 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from user_service import urls as user_urls
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='User API',
+        default_version='v1',
+        description='User API Documentation',
+        terms_of_service='https://www.google.com/policies/terms/',
+        contact=openapi.Contact(email="alif.arrizqy20@gmail.com"),
+        license=openapi.License(name="Test License"),
+    ),
+    public=True,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
+    # path('api-auth/', include('rest_framework.urls')),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('api/', include(user_urls))
 ]
