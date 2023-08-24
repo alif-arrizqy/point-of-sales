6b16aad2063df3b04d2cc8baf6eec058c6cd4b6f
alif-arrizqy <alif.arrizqy20@gmail.com>
2023-08-24 16:46:50 +0700

    remove unuse packages

--- user_app/user_service/views.py
+++ user_app/user_service/views.py
@@ -5,7 +5,7 @@ from rest_framework import status, permissions
 from .models import User
 from .serializers import UserSerializer
 from .helpers import ResponseHelper
-from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseNotFound
+
 
 
 class UserListView(APIView):
