1e13955f101a3d1aaad19641b174500341a3b169
alif-arrizqy <alif.arrizqy20@gmail.com>
2023-08-24 21:37:04 +0700

    refactor endpoint

--- user_app/user_service/urls.py
+++ user_app/user_service/urls.py
@@ -1,8 +1,13 @@
-from django.conf.urls import url
+# from django.conf.urls import url # linux
 from django.urls import path, include
-from .views import UserListView, UserDetailView
+from . import views
 
 urlpatterns = [
-    path('user/', UserListView.as_view()),
-    path('user/<user_id>/', UserDetailView.as_view()),
+    # path('user/', UserListView.as_view()),
+    # path('user/<user_id>/', views.UserDetailView.as_view()),
+    path('', views.list_users, name='get user'),
+    path('create/', views.create_user, name='create user'),
+    path('<user_id>/', views.get_user, name='get user by id'),
+    path('update/<user_id>/', views.update_user, name='update user'),
+    path('delete/<user_id>/', views.delete_user, name='delete user'),
 ]
