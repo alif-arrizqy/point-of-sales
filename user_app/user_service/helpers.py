ea0221fccf93f3b17a45d52482c9696c7e4005a1
alif-arrizqy <alif.arrizqy20@gmail.com>
2023-08-26 23:55:06 +0700

    change variable

--- user_app/user_service/helpers.py
+++ user_app/user_service/helpers.py
@@ -10,14 +10,14 @@ class ResponseHelper():
 
     def helper_response(self):
         return Response({
-            'status': self.status,
+            'status_code': self.status,
             'message': self.message,
             'data': self.data
         })
 
     def helper_response_without_data(self):
         return Response({
-            'status': self.status,
+            'status_code': self.status,
             'message': self.message,
         })
 
