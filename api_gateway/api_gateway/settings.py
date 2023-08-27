d72939abc039c1555f2188f3c166728d01279868
alif-arrizqy <alif.arrizqy20@gmail.com>
2023-08-27 12:36:34 +0700

    create endpoint parent api gateway and setting port

--- api_gateway/api_gateway/settings.py
+++ api_gateway/api_gateway/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'gateway_service',
+    'drf_yasg'
 ]
 
 MIDDLEWARE = [
