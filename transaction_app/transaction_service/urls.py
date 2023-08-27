a6257c621a989d80de4b2940d339d61afdbeaeaa
alif-arrizqy <alif.arrizqy20@gmail.com>
2023-08-27 16:46:11 +0700

    delete unused lines

--- transaction_app/transaction_service/urls.py
+++ transaction_app/transaction_service/urls.py
@@ -3,9 +3,5 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    # path('', views.list_items, name='list all items'),
     path('create/', views.create_transaction, name='create transaction'),
-    # path('<uuid>/', views.get_item, name='get item by uuid'),
-    # path('update/<uuid>/', views.update_item, name='update item'),
-    # path('delete/<uuid>/', views.delete_item, name='delete item'),
 ]
